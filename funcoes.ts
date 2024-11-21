4ec75c148c005ac24f024e960171c2f725939cb6
Hanna Sampaio <130690107+hannasampaio@users.noreply.github.com>
2024-11-21 10:01:53 +0000

    feature/multiplicar-e-saudar

---
+++ funcoes.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome:string): string {
+    return `Olá ${nome}`;
+}
+
+const resultadoMultiplicacao = multiplicar(7, 7);
+const saudacao = saudar("Hanna Sampaio");
+
+console.log(resultadoMultiplicacao);
+console.log(saudacao);
